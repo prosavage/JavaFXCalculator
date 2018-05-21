d48850db1f21a32d9fa9231fec53c977a4505d17
ProSavage <naman0311@gmail.com>
2018-05-21 00:08:41 -0500

    Graphing Mode Now Works!, supports multiple equations.

--- src/application/grapher/GrapherController.java
+++ src/application/grapher/GrapherController.java
@@ -3,18 +3,34 @@ package application.grapher;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodTextRun;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 
 public class GrapherController {
 
     @FXML
-    private HBox canvasBox;
+    private Canvas canvas;
+    @FXML
+    private TextField equation;
+
+    public void graph() {
+
+        if (equation.getCharacters().toString().isEmpty()) {
+            return;
+        } else {
+            drawGraph(equation.getCharacters().toString());
+        }
+    }
 
 
-    public void test() {
-        Canvas canvas = new Canvas(570,250);
-        canvasBox.getChildren().add(canvas);
+
+    public void drawGraph(String equation) {
+       // canvasBox.getChildren().clear();
+        //Canvas canvas = new Canvas(570,250);
+        //canvasBox.getChildren().add(canvas);
         double startX = canvas.getWidth()/2 * -1;
         double endX = startX * -1;
         double startY = canvas.getHeight()/2 * -1;
@@ -23,17 +39,50 @@ public class GrapherController {
         GraphicsContext grapher = canvas.getGraphicsContext2D();
         grapher.beginPath();
 
-        String equation = "x+1";
-        grapher.moveTo(startX,Evaluator.eval(equation.replace("x",startX + "")));
-        for (double i = startX; i < endX; i++) {
-            grapher.lineTo(startX,Evaluator.eval(equation.replace("x",String.valueOf(i))));
+        grapher.setLineWidth(1.0);
+        grapher.setFill(Color.GREEN);
+        grapher.setStroke(Color.GREEN);
+
+
+        double originX = canvas.getWidth()/2;
+        double originY = canvas.getHeight()/2;
+        //grapher.moveTo(startX,Evaluator.eval(equation.replace("x",startX + "")) + endY);
+
+
+
+        //draw guide lines
+        for (int i = 0; i < canvas.getWidth(); i ++) {
+            grapher.fillOval(i,originY,1,1);
+
+        }
+        for (int i = 0; i < canvas.getHeight(); i ++) {
+            grapher.fillOval(originX,i,1,1);
         }
 
 
+        boolean first = true;
+
+        for (double i = startX; i < endX; i++) {
+            if (first) {
+                grapher.fillOval(originX + i * -1,Evaluator.eval(equation.replace("x",String.valueOf(i))) + originY,1,1);
+                first = false;
+            }
+            grapher.lineTo(originX + i * -1,Evaluator.eval(equation.replace("x",String.valueOf(i))) + originY);
+            grapher.stroke();
+            //grapher.fillOval(originX + i * -1,Evaluator.eval(equation.replace("x",String.valueOf(i))) + originY,1,1);
+        }
+
+        //grapher.fillRect(0,0,50,50);
 
 
 
 
+
+
+    }
+
+    public void clear() {
+        canvas.getGraphicsContext2D().clearRect(0,0,canvas.getWidth(),canvas.getHeight());
     }
 
 
--- src/application/grapher/GrapherScene.java
+++ src/application/grapher/GrapherScene.java
@@ -10,17 +10,17 @@ public class GrapherScene {
     public GrapherScene() {
         try {
             Stage grapherStage = new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("grapher.fxml"));
-            Scene scene = new Scene(root, 600, 400);
-            //scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+            FXMLLoader loader = new FXMLLoader();
+            Parent root = loader.load(getClass().getResourceAsStream("grapher.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("grapher.css").toExternalForm());
             grapherStage.setScene(scene);
             grapherStage.setTitle("Grapher");
             grapherStage.show();
+            GrapherController controller = (GrapherController) loader.getController();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        GrapherController grapher = new GrapherController();
-        grapher.test();
 
 
     }
